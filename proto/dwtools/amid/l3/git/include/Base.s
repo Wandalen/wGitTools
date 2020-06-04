7c252822cfb605a20d13470dc619f75e753ae5e9
wandalen <wandalen.me@gmail.com>
2020-06-04 11:49:22 +0300

    .

--- proto/dwtools/amid/l3/git/include/Base.s
+++ proto/dwtools/amid/l3/git/include/Base.s
@@ -6,13 +6,13 @@
 
 if( typeof module !== 'undefined' )
 {
-
   let _ = require( '../../../../../dwtools/Tools.s' );
 
   _.include( 'wCopyable' );
   _.include( 'wAppBasic' );
   _.include( 'wFiles' );
 
+  module[ 'exports' ] = _global_.wTools;
 }
 
 })();
