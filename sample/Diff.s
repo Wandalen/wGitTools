0d6fa034fd859fc0c2714be3249a30677ebc5b91
Jackie <jackshel45@gmail.com>
2020-07-17 23:32:14 +0300

    test commit 6

--- sample/Diff.s
+++ sample/Diff.s
@@ -6,8 +6,8 @@ var got =  _.git.diff
 ({ 
   localPath : _.path.join( __dirname, '..' ),
   state1 : 'working',
-  state2 : 'version::v0.3.36',
-  attachOriginalDiffOutput : 0,
+  state2 : 'tag::v0.3.36',
+  attachOriginalDiffOutput : 1,
   detailing : 1,
   explaining : 1
 });
