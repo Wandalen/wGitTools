8a1a68fb2219f18398e321943351739bef96caf3
volodymyrmigdal <volodymyr.migdal@gmail.com>
2020-07-23 10:56:22 +0300

    repair sample Diff

--- sample/Diff.s
+++ sample/Diff.s
@@ -6,7 +6,7 @@ var got =  _.git.diff
 ({
   localPath : _.path.join( __dirname, '..' ),
   state1 : 'working',
-  state2 : 'tag::v0.3.36',
+  state2 : '!v0.3.36',
   attachOriginalDiffOutput : 0,
   detailing : 1,
   explaining : 1,
